a9843955b6a0b9c54ffee15cb422f697d4aef7ab
Connor Lindsell <connor.lindsell@student.uts.edu.au>
2024-10-11 15:38:37 +1100

    Working LabBot and Teach
    DH parameters need to be changed but for the time being this is fine

--- @LabBot_7DOF/LabBot_7DOF.m
+++ @LabBot_7DOF/LabBot_7DOF.m
@@ -25,8 +25,8 @@ classdef LabBot_7DOF < RobotBaseClass
         function CreateModel(self)   
             % Create the UR10 model mounted on a linear rail
             %DH = [THETA D A ALPHA SIGMA]
-            link(1) = Link([0    0           0        -pi/2    1]); % PRISMATIC Link
-            link(2) = Link([-pi/2     0.169      0        pi/2   1]);
+            link(1) = Link([pi    0           0        pi/2    1]); % PRISMATIC Link
+            link(2) = Link([0     0.1697      0        -pi/2   0]);
             link(3) = Link([0     0.176       0.6129   -pi     0]);
             link(4) = Link([0     0.12781     0.5716	pi      0]);
             link(5) = Link([0     0.1157      0        -pi/2	0]);
@@ -35,8 +35,8 @@ classdef LabBot_7DOF < RobotBaseClass
         
             
             % Incorporate joint limits
-            link(1).qlim = [0.8   0.01]; % Must be negative
-            link(2).qlim = [0.8   0.01];
+            link(1).qlim = [0.8   0.01]; 
+            link(2).qlim = [-360 360]*pi/180;
             link(3).qlim = [-90 90]*pi/180;
             link(4).qlim = [-170 170]*pi/180;
             link(5).qlim = [-360 360]*pi/180;
--- LabBot_7DOF_TeachFunction.m
+++ LabBot_7DOF_TeachFunction.m
@@ -4,9 +4,6 @@ function LabBot_7DOF_TeachFunction()
     % Instantiate the robot
     robot = LabBot_7DOF(); 
 
-    % Plot the robot at its home configuration
-    robot.model.plot(robot.homeQ); 
-
     % Pass the current joint state when calling teach
     currentJointState = robot.model.getpos();  % Get the current joint configuration
     robot.model.teach(currentJointState);  % Pass it to teach
