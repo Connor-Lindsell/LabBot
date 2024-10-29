1d6156eb267a425d3dd375f90c3721e470e09c4d
willi <william4014hsu@gmail.com>
2024-10-30 06:34:55 +1100

    Change position of safety items

--- LabBotEnvironment.m
+++ LabBotEnvironment.m
@@ -34,8 +34,8 @@ classdef LabBotEnvironment < handle
             camlight;
             
             %% Static Object Initialisation
-            PlaceObject('fireExtinguisher.ply', [2,2,0]);
-            PlaceObject('emergencyStopWallMounted.ply', [1,2,0])
+            PlaceObject('fireExtinguisher.ply', [2,1.5,0]);
+            PlaceObject('emergencyStopWallMounted.ply', [-0.5,1.2,0.5])
             PlaceObject('TPose.ply', [0,1.9,0]);
             PlaceObject('BMan.ply', [0,1.5,0])
             % PlaceTransparentObject('TPose.ply', [0,0,1]); % Ghost
