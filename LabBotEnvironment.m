0fe4bac178b7ea9f1e76f6bf1d2be28ab38e741b
Connor Lindsell <connor.lindsell@student.uts.edu.au>
2024-10-30 08:37:44 +1100

    merge

--- LabBotEnvironment.m
+++ LabBotEnvironment.m
@@ -32,15 +32,24 @@ classdef LabBotEnvironment < handle
             grid on;  % Display a grid for better visualization of object positions
             hold on;  % Keep the plot active for additional elements
             camlight;
+
+            %% Ground Texture
+            surf([-5,-5;5,5] ...
+            ,[-5,5;-5,5] ...
+            ,[0.01,0.01;0.01,0.01] ...
+            ,'CData',imread('concrete.jpg') ...
+            ,'FaceColor','texturemap');
             
             %% Static Object Initialisation
-            PlaceObject('fireExtinguisher.ply', [2,2,0]);
-            PlaceObject('emergencyStopWallMounted.ply', [1,2,0])
-            PlaceObject('TPose.ply', [0,1.9,0]);
+            PlaceObject('fireExtinguisher.ply', [2,1.5,0]);
+            PlaceObject('emergencyStopWallMounted.ply', [-0.5,1.2,0.5])
+            PlaceObject('TPose.ply', [0.2,1.9,0]); %% https://creazilla.com/media/3d-model/1795171/scientist
             PlaceObject('BMan.ply', [0,1.5,0])
             % PlaceTransparentObject('TPose.ply', [0,0,1]); % Ghost
 
             PlaceObject('Enclosure.ply', [0,0,0])
+            PlaceObject('LoudSpeaker.ply', [0,0,0]) %% https://free3d.com/3d-model/speaker-99850.html
+            PlaceObject('SignalTower.ply', [0,0,0]) %% https://grabcad.com/library/signal-tower-1
             PlaceTransparentObject('Window.ply',[0,0,0]);
             PlaceTransparentObject('Door.ply',[0,0,0]); % Door Closed
             % PlaceTransparentObject('Door.ply',[1.8,0,0]); % Door Opened
